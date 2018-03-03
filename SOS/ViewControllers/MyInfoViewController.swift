546ff389b08c8d0f9a7157d784db0e53878302b0
Ashlee Krammer <ashlee.alyse@me.com>
2018-03-03 14:07:12 -0500

    Snapkit for My Info View

--- SOS/ViewControllers/MyInfoViewController.swift
+++ SOS/ViewControllers/MyInfoViewController.swift
@@ -10,10 +10,41 @@ import UIKit
 
 class MyInfoViewController: UIViewController {
 
+    let myInfoView = MyInfoView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.addSubview(myInfoView)
+        myInfoView.tableView.delegate = self
+        myInfoView.tableView.dataSource = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(editButton))
+//        myInfoView.tableView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellReuseIdentifier: <#T##String#>)
     }
 
+    @objc private func editButton() {
+        
+    }
+    
 
+}
+
+extension MyInfoViewController: UITableViewDelegate {
+    
+}
+
+extension MyInfoViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: <#T##String#>, for: indexPath)
+        return UITableViewCell()
+    }
+    
 }
