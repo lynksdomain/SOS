d23b7187848032213679b8be809188aeee41499d
Masai Young <masai.young@gmail.com>
2018-03-04 03:39:06 -0500

    Add site details to siteDetailVC

--- SOS/ViewControllers/LoginViewController.swift
+++ SOS/ViewControllers/LoginViewController.swift
@@ -10,8 +10,23 @@ import UIKit
 import SnapKit
 
 class LoginViewController: UIViewController {
+    enum PasswordPin: String {
+        case one = "1"
+        case two = "2"
+        case three = "3"
+        case four = "4"
+        case five = "5"
+        case six = "6"
+        case seven = "7"
+        case eight = "8"
+        case nine = "9"
+        case zero = "0"
+        case clear = "Clear"
+        case delete = "X"
+    }
+    
     let contentView = LoginView()
-    let model = ["1","2","3","4","5","6","7","8","9","0", "Clear", "X"]
+    let enumModel: [PasswordPin] = [.one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .zero, .clear, .delete]
     let correctPassword = "9999"
     var password = "" {
         didSet {
@@ -68,39 +83,33 @@ extension LoginViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let cell = collectionView.cellForItem(at: indexPath)! as! LoginCollectionViewCell
         guard let labelText = cell.numberLabel.text else { return }
-        
-        if contentView.passwordView.text != nil {
-            if let numericalText = Int(labelText) {
-                contentView.passwordView.text! += numericalText.description
-                self.password = contentView.passwordView.text!
-            }
-            
-            if labelText == "X" {
+        guard let pin = PasswordPin.init(rawValue: labelText) else { return }
+        guard let passwordInput = contentView.passwordView.text else { return }
+            switch pin {
+            case .clear:
+                clearPasswordField()
+            case .delete:
                 if !(contentView.passwordView.text?.isEmpty)! {
                     contentView.passwordView.text?.removeLast()
-                    self.password = contentView.passwordView.text!
+                    self.password = passwordInput
                 }
-            }
-            
-            if labelText == "Clear" {
-                contentView.passwordView.text?.removeAll()
+            default:
+                contentView.passwordView.text! += pin.rawValue
                 self.password = contentView.passwordView.text!
             }
-        }
-        
         
     }
 }
 
 extension LoginViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return model.count
+        return enumModel.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "feedCell", for: indexPath) as! LoginCollectionViewCell
         cell.backgroundColor = .blue
-        cell.numberLabel.text = model[indexPath.row].description
+        cell.numberLabel.text = enumModel[indexPath.row].rawValue
         return cell
     }
     
--- SOS/ViewControllers/SiteDetailViewController.swift
+++ SOS/ViewControllers/SiteDetailViewController.swift
@@ -9,27 +9,79 @@
 import UIKit
 
 class SiteDetailViewController: UIViewController {
-
+    
+    let contentView = SiteDetailView()
+    var site: TestSite!
+    var sections: [[String]]!
+    var titles: [[String]]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        prepareContentView()
+        prepareDelegates()
+        dump(site.location)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    convenience init(site: TestSite) {
+        self.init()
+        self.site = site
+        sections = [site.siteInfo, site.location, site.contact, site.hours, site.cost]
+        titles = [site.siteInfoTitles, site.locationTitles, site.contactTitles, site.hoursTitles, site.costTitles]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func prepareContentView() {
+        view.addSubview(contentView)
+        contentView.snp.makeConstraints { make in
+            make.edges.equalTo(view.snp.edges)
+        }
     }
-    */
+    
+    private func prepareDelegates() {
+        contentView.tableView.delegate = self
+        contentView.tableView.dataSource = self
+    }
+}
 
+extension SiteDetailViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let titles = ["Name", "Location", "Contact", "Hours", "Cost"]
+        return titles[section]
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        view.tintColor = Stylesheet.Colors.LightPink
+        let header = view as! UITableViewHeaderFooterView
+        header.textLabel?.textColor = UIColor.white
+    }
+}
+
+extension SiteDetailViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let siteCategory = Array(site.detailDict.keys)[section]
+        let categoryDict = site.detailDict[siteCategory]!
+        let onlyPresentFields = Array(categoryDict.values.flatMap({$0}))
+        return sections[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailCell", for: indexPath)
+        
+//        let siteCategory = Array(site.detailDict.keys)[indexPath.section]
+//        let categoryDict = site.detailDict[siteCategory]
+//        let categoryKeys = Array(categoryDict!.keys)
+//        let key = categoryKeys[indexPath.row]
+//        let value = categoryDict![key] ?? "Mistake"
+        
+        
+        let data = sections[indexPath.section][indexPath.row]
+        let title = titles[indexPath.section][indexPath.row]
+        cell.textLabel?.text = title + "  :  " + data
+        return cell
+    }
+    
+    
 }
